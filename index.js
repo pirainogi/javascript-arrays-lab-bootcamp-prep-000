3dcfb42d9e3cf804e7abfdfcd44dd7621dfff05f
Gabbie Piraino <pirainogi@gmail.com>
2018-11-04 22:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,10 +30,12 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function appendKitten(name){
   kittens.splice(3, 0, "Broom"); 
+  return kittens; 
 }
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function prependKitten(name){
   kittens.splice(0, 0, "Arnold")
+  return kittens; 
 }
